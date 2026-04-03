8f659c8bc8f102c93514ec8524295c194e1721be
ljdusaban <leojohndusaban@gmail.com>
2026-04-03 23:31:57 +1000

    Remove tasks

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,7 +16,6 @@ const Navbar = () => {
       <div>
         {user ? (
           <>
-            <Link to="/tasks" className="mr-4">CRUD</Link>
             <Link to="/profile" className="mr-4">Profile</Link>
             <button
               onClick={handleLogout}
