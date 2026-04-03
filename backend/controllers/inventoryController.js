7dcaa78958fce826b854e03425f52ca328f81f4f
ljdusaban <leojohndusaban@gmail.com>
2026-04-03 21:03:13 +1000

    View product function controller and routes

--- backend/controllers/inventoryController.js
+++ backend/controllers/inventoryController.js
@@ -48,4 +48,29 @@ const createProduct = async (req, res) => {
   }
 };
 
-module.exports = { createProduct };
+
+//View Products
+const getProducts = async (req, res) => {
+try {
+const products = await Product.find({ createdBy: req.user.id }).sort({ createdAt: -1 });
+return res.status(200).json(products);
+} catch (error) {
+return res.status(500).json({ message: error.message });
+}
+};
+
+const getProductById = async (req, res) => {
+try {
+const product = await Product.findOne({ _id: req.params.id, createdBy: req.user.id });
+  if (!product) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+
+  return res.status(200).json(product);
+} catch (error) {
+  return res.status(500).json({ message: error.message });
+}
+};
+
+
+module.exports = { createProduct, getProducts, getProductById };
--- backend/routes/inventoryRoutes.js
+++ backend/routes/inventoryRoutes.js
@@ -1,9 +1,11 @@
 
 const express = require('express');
-const { createProduct } = require('../controllers/inventoryController');
+const { createProduct, getProducts, getProductById } = require('../controllers/inventoryController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/products', protect, createProduct);
+router.get('/products', protect, getProducts);
+router.get('/products/:id', protect, getProductById);
 
 module.exports = router;
